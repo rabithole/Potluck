b07173948fe0ddf0654bb8bcdd3a6a1c895a908c
Robert Kulp <pedalinon@gmail.com>
2019-11-13 14:19:17 -0800

    Adjusted notes

--- Potluck_planner.js
+++ Potluck_planner.js
@@ -24,9 +24,10 @@ PITCH
 If you have ever tried to organize a potluck through text messages, online to-do lists or spreadsheets you get why this app is important. In the world of social gatherings and potlucks the "Potluck Planner" is king. This is your place for all things pot luck. 
 
 MVP
-1 As an organizer I can create an upcoming potluck and invite my friends to attend. 
+1. As an organizer I can create an upcoming potluck and invite my friends to attend. 
 2. As an organizer I can adjust dates, times and locations of the potluck. 
-3.As an organize I can use the list feature in my app to add food items that we'd like to see at the potluck. 
+3. As an organizer I can use the list feature in my app to add food items that we'd like to see at the potluck. 
+
 4. As a guest to a potluck I want to be able to confirm that I'm going to the upcoming event. 
 5. As a guest I'd like to be able to select which items I'd like to be responsible for bringing. 
 
