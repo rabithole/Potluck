1d2ba640ba8a80b4f1d8a0d1b8ad5b7a7cc43dad
Robert Kulp <pedalinon@gmail.com>
2019-11-12 18:27:19 -0800

    initialize

---
+++ Potluck_planner.js
@@ -0,0 +1,41 @@
+Potluck planner
+
+PROJECT NAME
+Potluck planner
+
+WEB UI DEVELOPER
+Xaver Pinero
+Ahmad Sayadi
+Mason Parker
+
+WEB REACT I
+Jerry McGarity
+
+WEB REACT II
+Robert Kulp
+
+BACKEND DEVELOPER
+None
+
+PROJECT LEAD
+Larry Simiyu (WEBPT8)
+
+PITCH
+If you have ever tried to organize a potluck through text messages, online to-do lists or spreadsheets you get why this app is important. In the world of social gatherings and potlucks the "Potluck Planner" is king. This is your place for all things pot luck. 
+
+MVP
+1 As an organizer I can create an upcoming potluck and invite my friends to attend. 
+2. As an organizer I can adjust dates, times and locations of the potluck. 
+3.As an organize I can use the list feature in my app to add food items that we'd like to see at the potluck. 
+4. As a guest to a potluck I want to be able to confirm that I'm going to the upcoming event. 
+5. As a guest I'd like to be able to select which items I'd like to be responsible for bringing. 
+
+NOTE: All of the user stories above should only require a single user type. Users can create "potlucks" and add other users to them.
+
+STRETCH
+Add a reminders functionality to product that allows users to link up their upcoming dates to their gcal. 
+
+COLOR SCHEME
+Red #F96E84
+Orange #EFB39B
+Green #1A5763
